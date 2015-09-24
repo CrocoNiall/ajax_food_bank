8067c6ca3fa83a9c377debee7dadbb19d5efbde9
Niall Wallace <nj.wallace@outlook.com>
2015-09-24 18:53:25 +0100

    added delete function to API

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.static(__dirname + '/public'))
 
 // body parser config
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // DATA //
 
@@ -23,6 +24,8 @@ var foods =[
   {id: 4, name: "Kale", yumminess: "meh"}
 ]
 
+
+
 // ROUTES //
 
 // root path
@@ -31,19 +34,39 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + '/public/views/index.html'))
 })
 
+//INDEX
 // foods index path
 app.get("/foods", function (req, res) {
   // render foods index as JSON
+  res.json(foods)
+
 })
 
+//CREATE
 app.post("/foods", function (req, res) {
+
+  res.json(req.body)
+  var jsonResponse = req.body
+  foods.push(jsonResponse)
+
+
   // add a unique id
   // add new food to DB (array, really...)
   // send a response with newly created object
 })
 
+//DELETE
 app.delete("/foods/:id", function (req, res) {
   console.log("hitting delete route");
+  var position
+  for (var i = 0; i < foods.length -1; i++){
+    foods[i].id.toString() === req.params.id ? position = i : position 
+    console.log(foods[i]);
+  }
+  res.json(foods[i])
+  foods.splice(position, 1)
+  
+
   // finding an object with id = req.body.id out of the foods
   // remove item from array
   // render deleted object
