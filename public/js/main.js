9190b1b6b8bbb05e2fe80c97b3414b7643bfe590
Niall Wallace <nj.wallace@outlook.com>
2015-09-24 21:01:14 +0100

    added event listeners on button and calling delete method

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,34 @@ console.log('Hello Niall')
     render(response);
   });
 
+
+
 })
+
 
 
 function render(response){
   console.log(response)
+
+  for (var i = 0; i < response.length -1; i++){
+    var foodItem = '<li data-id=' + response[i].id + '>'
+    foodItem += '<b>' + response[i].name + '</b>'
+    foodItem += ' Rateing: ' + '<i>' + response[i].yumminess + '</i>'
+    foodItem += '</li>'
+    foodItem += '<button class="deleteButton" name="subject" type="submit" value="' + response[i].id +'">Delete</button><br><br>'
+    console.log(foodItem)
+    addToPage(foodItem);
+  }
+}
+
+
+function addToPage(item){
+  var results = $('#food-list');
+  results.hide().append(item).fadeIn('slow');
+
+  $('.deleteButton').on('click', function(event){
+
+    deleteRecord(this.value);
+  })
+
 }
