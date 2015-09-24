953a30d45e431d12a76f815a4fdb8da4edee64fd
Niall Wallace <nj.wallace@outlook.com>
2015-09-24 23:34:55 +0100

    updated comments and indentation'

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 console.log('Hello Niall')
-
+//Send ajax GET request
   $.get('http://localhost:3000/foods', function(response) {
     render(response);
   });
@@ -11,7 +11,7 @@ var form = $('#addFood')
   form.on('submit', function(){
     event.preventDefault()
 
-
+//Send ajax DELETE request
       $.ajax({
       url: '/foods',
       type: 'post',
@@ -21,22 +21,20 @@ var form = $('#addFood')
           console.log(result)
       }
   });
-
 })
-
-
 
 function render(response){
   console.log(response)
-
+//build DOM Element
   for (var i = 0; i < response.length ; i++){
     var foodItem = '<li id="' + response[i].id + '">'
     foodItem += '<b>' + response[i].name + '</b>'
-    foodItem += ' Rateing: ' + '<i>' + response[i].yumminess + '</i>'
+    foodItem += ' -- Rateing: ' + '<i>' + response[i].yumminess + '</i>'
     foodItem += '<button id="' + response[i].id +'" name="subject" type="submit" value="' + response[i].id +'">Delete</button><br><br>'
     foodItem += '</li>'
     var id = response[i].id
-    console.log(foodItem)
+
+//call function to add element to DOM
     addToPage(foodItem, id);
   }
 }
@@ -47,31 +45,32 @@ function addToPage(item, id){
   results.hide().append(item).fadeIn('slow');
 
   $('#' + id).on('click', function(event){
-    
+
+//Call method to remove record from DB
     removeRecord(this.value, id);
   })
 
 
 
 function removeRecord(valueId, id) {
+//remove element from DOM
   $('#' + id).slideUp('slow')
+
+//Call Method to delete record from DB
   deleteRecord(id)
   console.log('deleting' + id)
   }
 
 
 function deleteRecord(id){
+//send Ajax request to delete record form db
   $.ajax({
     url: '/foods/' + id,
     type: 'DELETE',
     success: function(result) {
         console.log(result)
     }
-});
+  });
 }
-
-
-
-
 
 }
